ee7f2fac9024e67009c5eb1b762b0c11f80a0e82
Logan Gore <119902778+logan-stytch@users.noreply.github.com>
2025-09-26 11:35:27 -0700

    Mark nullable params properly (#14)

--- src/Core/Client.php
+++ src/Core/Client.php
@@ -21,9 +21,9 @@ class Client
     public function __construct(
         string $projectId,
         string $secret,
-        string $environment = null,
-        string $fraudEnvironment = null,
-        string $customBaseUrl = null
+        ?string $environment = null,
+        ?string $fraudEnvironment = null,
+        ?string $customBaseUrl = null
     ) {
         $this->projectId = $projectId;
         $this->secret = $secret;
